b763239c5021a57f675ab8f2bc22d967556e2d98
mposypkin <mposypkin@gmail.com>
2021-10-11 10:48:56 +0300

    Added Subproblem as a class

--- main.py
+++ main.py
@@ -5,12 +5,27 @@ import interval as ival
 from sortedcontainers import SortedList
 from sortedcontainers import SortedKeyList
 
-subs = [{"chr" : - 1.0, "dat" : 1}, {"chr" : - 1.2, "dat" : 2}]
+class Sub:
+    def __init__(self, level, bound, data):
+        self.level = level
+        self.bound = bound
+        self.data = data
+
+    def __repr__(self):
+        return "[ level = " + str(self.level) + ", bound = " + str(self.bound) + ", data =  " + str(self.data) + "]"
+
+I1 = ival.Interval([-1,1])
+s1 = Sub(1,-1,I1)
+print(s1)
+I2 = ival.Interval([-1,2])
+s2 = Sub(3,-1.1,I2)
+subs = [s1, s2]
 print (subs)
-sl = SortedKeyList(key = lambda x : -x["chr"])
+# sl = SortedKeyList(key = lambda s : -s.bound)
+sl = SortedKeyList(key = lambda s : s.level)
 sl.update(subs)
 print(sl)
-sl.add({"chr" : - 1.5, "dat" : 3})
+sl.add(Sub(2, 2, ival.Interval([-2,1])))
 print(sl)
 while len(sl) > 0:
     print(sl.pop())
