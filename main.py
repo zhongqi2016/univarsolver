dadf5dce41ad3b08f07b548afe41517d7bce01fd
mposypkin <mposypkin@gmail.com>
2021-10-16 11:06:18 +0300

    Added reading test functions from a csv file

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import math
-import sympy
+import sympy as sym
+import pandas as pd
 import interval as ival
 
 from sortedcontainers import SortedList
 from sortedcontainers import SortedKeyList
+
+
+def read_problems(fname):
+    data = pd.read_csv(fname, index_col = 'name', comment = '#')
+    return data
+
+
+def make_objective(func):
+    sf = sym.sympify(func)
+    print(sf)
+    x = sym.symbols('x')
+    f = sym.lambdify(x, sf)
+    print(f(2))
+
+
 
 class Sub:
     def __init__(self, level, bound, data):
@@ -29,4 +45,12 @@ sl.add(Sub(2, 2, ival.Interval([-2,1])))
 print(sl)
 while len(sl) > 0:
     print(sl.pop())
+problems = read_problems("problems.csv")
+print(problems)
+make_objective(problems.loc['prob1','objective'])
+# print(problems.objective)
+# print(problems.loc[lambda df : df['name'] == 'prob1'])
+# df1 = problems.loc[lambda df : df['name'] == 'prob1']
+# print(df1.loc[0,'objective'])
+# make_objective(df1.loc[0,'objective'])
 # print ("Hello")
