d93098fe11f7fb09e09d2660caecaa87f15719d0
mposypkin <mposypkin@gmail.com>
2021-11-18 10:15:18 +0300

    added grid seach

---
+++ gridsearch.py
@@ -0,0 +1,22 @@
+import numpy as np
+def grid_search(f, a, b, s):
+    """
+    Searches for a minimum on a uniform grid
+    Args:
+        f: objective function
+        a: left interval end
+        b: right interval end
+        s: step size
+
+    Returns:
+        found minimum as a tuple (x*, f(x*))
+    """
+    points = np.arange(a, b, s)
+    xr = np.nan
+    fr = np.nan
+    for x in points:
+        fn = f(x)
+        if np.isnan(fr) or fn < fr:
+            xr = x
+            fr = fn
+    return xr, fr
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import uvarprob as uvpr
 import bnb as bnb
 import sub as sub
 import ivalprocessor as ivproc
+import gridsearch as gs
 
 # from sortedcontainers import SortedList
 from sortedcontainers import SortedKeyList
@@ -18,9 +19,9 @@ def read_problems(fname):
 problems = read_problems("problems.csv")
 print(problems)
 name = 'shekel1'
-prob = uvpr.UniVarProblem(name, problems.loc[name,'objective'], problems.loc[name,'a'], problems.loc[name,'b'], problems.loc[name,'min_f'], problems.loc[name,'mins_x'])
+prob = uvpr.UniVarProblem(name, problems.loc[name,'objective'], problems.loc[name,'a'], problems.loc[name,'b'], problems.loc[name,'min_f'], problems.loc[name,'mins_x'].strip('][').split(', '))
 print(prob)
-# problems.to_csv('/tmp/prob.csv')
+
 dp = ivproc.IntervalProcessor(prob.min_f, prob.mins_x[0], prob, 0.0001)
 
 sl = SortedKeyList(key = lambda s : s.level)
@@ -32,6 +33,17 @@ cnt = 1000
 print("steps performed: ", bnb.bnb(sl, cnt, dp))
 print("Record value = ", dp.rec_v, " at ", dp.rec_x);
 print(sl)
+
+true_min = gs.grid_search(prob.objective, prob.a, prob.b, 1e-3)
+print("Grid search:", true_min)
+prob.mins_x = [true_min[0]]
+prob.min_f = true_min[1]
+print("problem = ", prob)
+print("type = ", type(problems.loc[name,'mins_x']))
+print("type = ", type(prob.mins_x))
+problems.loc[name,'mins_x'] =  str([round(true_min[0], 5), 1.])
+problems.loc[name,'min_f'] = round(true_min[1], 5)
+problems.to_csv('/tmp/prob.csv')
 
 # while len(sl) > 0:
 #     print(sl.pop())
