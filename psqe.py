34c65320aed036fdeb5a4f897f2be33128ff0e4a
mposypkin <mposypkin@gmail.com>
2021-12-08 13:39:25 +0300

    Debugged PSQE and added notebook to demonstrate the program.

--- psqe.py
+++ psqe.py
@@ -28,8 +28,12 @@ class PSQE:
         self.dfb = df(b)
 
         delt = (self.dfb - self.dfa - alp * (b - a))/(bet - alp)
-        self.c = ((delt - alp) * self.dfa + (bet - delt) * self.dfb + 0.5 * delt * delt * (bet - alp) + alp * delt * (b - a) + 0.5 * alp * (a * a - b * b) + self.fa - self.fb)/(delt * (bet - alp))
+        print("delt = ", delt)
+        self.c = ((delt - a) * self.dfa + (b - delt) * self.dfb + 0.5 * delt**2 * (bet - alp) + alp * delt * (b - a) + 0.5 * alp * (a**2 - b**2) + self.fa - self.fb)/(delt * (bet - alp))
         self.d = self.c + delt
+
+    def __repr__(self):
+        return "Estimator " + "a = " + str(self.a) + ", b = " + str(self.b) + ", c = " + str(self.c) + ", d = " + str(self.d) + ", alp = " + str(self.alp) + ", bet = " + str(self.bet) + ", fa = " + str(self.fa) + ", fb = " + str(self.fb) + ", dfa = " + str(self.dfa) + ", dfb = " + str(self.dfb)
 
     def underestimator(self, x):
         """
@@ -42,6 +46,6 @@ class PSQE:
         if x < self.c:
             return self.fa + self.dfa * (x - self.a) + 0.5 * self.alp * (x - self.a)**2
         elif x < self.d:
-            return self.fa + self.dfa * (self.c - self.a) + 0.5 * self.alp * (self.c - a)**2 + (self.dfa + self.alp * (self.c - self.a))*(x - self.c) + 0.5 * self.bet * (x - self.c)**2
+            return self.fa + self.dfa * (self.c - self.a) + 0.5 * self.alp * (self.c - self.a)**2 + (self.dfa + self.alp * (self.c - self.a))*(x - self.c) + 0.5 * self.bet * (x - self.c)**2
         else:
-            self.fb + self.dfb * (x - self.b) + 0.5 * self.alp * (x - self.b)**2
+            return self.fb + self.dfb * (x - self.b) + 0.5 * self.alp * (x - self.b)**2
